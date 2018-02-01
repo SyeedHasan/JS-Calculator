fec2d7369a6fb1a429d0d4d1ba47b45697b17ccb
SyeedHasan <naqvi_hasan@live.com>
2018-02-01 21:48:36 +0500

    removed mutltiple sign allowance

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ let eight = document.getElementById('eight');
 let nine = document.getElementById('nine');
 let zero = document.getElementById('zero');
 
+// Whether an answer has been calculated or not.
+let answerState = false;
+// Identify the number/symbol to avoid repitition.
+let identifyValue = "number";
+
 let numbers = document.querySelectorAll('.number'); //This selects all numbers
-let symbols = document.querySelectorAll('symbols'); //This selects all symbols
+let symbols = document.querySelectorAll('.symbols'); //This selects all symbols
 
 var display = document.querySelector('#displayBox');
 var answer = document.querySelector('#equal');
@@ -18,11 +23,46 @@ var lastDigit = "number";
 //Turn this into 'symbol' if a symbol is experienced.
 
 function myFunction(obj) {
-    console.log(obj.value);
-    display.value += obj.value;
+    // if (answerState == true) {
+    //     display.value = "";
+    //     answerState = false;
+    // }
+    // display.value += obj.value;
+
+    // If a value has previously been calculated, empty the input cell.
+// This function runs on each click.
+    if (answerState == true) {
+        display.value = "";
+        answerState = false;
+    }
+
 };
 
-answer.addEventListener('click', function(){
+answer.addEventListener('click', function () {
     var ans = eval(display.value);
     display.value = ans;
+    answerState = true;
 });
+
+Array.from(numbers).forEach(function (number) {
+    number.addEventListener('click', function (num) {
+        display.value += num.path[0].value;
+        identifyValue = "number";
+    });
+
+});
+
+Array.from(symbols).forEach(function (symbol) {
+    symbol.addEventListener('click', function (sym) {
+        if (identifyValue !== "symbol" && symbol.value !== "=") {
+            //A symbol has been used.
+            identifyValue = "symbol";
+            display.value += symbol.value;
+        }
+    });
+
+});
+
+
+
+
