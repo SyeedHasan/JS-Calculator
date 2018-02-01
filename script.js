29057f88610011e0ca56885b7dab6575d3b6acd7
SyeedHasan <naqvi_hasan@live.com>
2018-02-01 21:01:42 +0500

    added eval func

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 let one = document.getElementById('one');
 let two = document.getElementById('two');
 let three = document.getElementById('three');
-var four = document.querySelectorAll("#four");
+let four = document.querySelectorAll("#four");
 let five = document.getElementById('five');
 let six = document.getElementById('six');
 let seven = document.getElementById('seven');
 let eight = document.getElementById('eight');
 let nine = document.getElementById('nine');
 let zero = document.getElementById('zero');
-var display = document.querySelector('td#displayBox');
 
-// four.addEventListener('click', function(){
-//     this.innerHTML = "four";
-// });
+let numbers = document.querySelectorAll('.number'); //This selects all numbers
+let symbols = document.querySelectorAll('symbols'); //This selects all symbols
 
+var display = document.querySelector('#displayBox');
+var answer = document.querySelector('#equal');
+var lastDigit = "number";
+//Turn this into 'symbol' if a symbol is experienced.
 
 function myFunction(obj) {
     console.log(obj.value);
-    display.innerHTML += obj.value;
+    display.value += obj.value;
 };
 
+answer.addEventListener('click', function(){
+    var ans = eval(display.value);
+    display.value = ans;
+});
